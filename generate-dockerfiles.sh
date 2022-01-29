03065af7bc94e89f9e57e4fe13a25eb49f277b48
bfren <ben@bcgdesign.com>
2022-01-29 08:22:30 +0000

    v1.3.4 (#13)
    Minor updates
    * Make bf- executables executable from install script
    
    Build updates
    * Using version-specific overlays for customisation
    * Moving ESH_BUILD to root
    
    Documentation updates
    * Updating copyright year to 2022

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-ALPINE_VERSIONS="3.12 3.13 3.14 3.15 edge"
+ALPINE_VERSIONS="3.8 3.12 3.13 3.14 3.15 edge"
 for V in ${ALPINE_VERSIONS} ; do
 
     echo "Alpine ${V}"
