52fe51d2f834e965af99689882a1f2c0018a05f7
bfren <bf@bfren.dev>
2023-05-10 15:38:38 +0100

    Adding Alpine 3.18

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-ALPINE_VERSIONS="3.8 3.9 3.10 3.11 3.12 3.13 3.14 3.15 3.16 3.17 edge"
+ALPINE_VERSIONS="3.8 3.9 3.10 3.11 3.12 3.13 3.14 3.15 3.16 3.17 3.18 edge"
 for V in ${ALPINE_VERSIONS} ; do
 
     echo "Alpine ${V}"
