60c1a5739b527441da3afc0a6de774d7c498cccf
bfren <bf@bfren.dev>
2023-10-21 08:34:31 +0100

    Adding build log to Dockerfiles

--- generate.sh
+++ generate.sh
@@ -4,38 +4,34 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-ALPINE_VERSIONS="3.15 3.16 3.17 3.18"
+ALPINE_EDITIONS="3.15 3.16 3.17 3.18"
 NUSHELL_VERSION="0.86.0"
-for V in ${ALPINE_VERSIONS} ; do
+for E in ${ALPINE_EDITIONS} ; do
 
-    echo "Alpine ${V}"
-    ALPINE_REVISION=`cat ./${V}/ALPINE_REVISION`
+    echo "Alpine ${E}"
+    ALPINE_VERSION=`cat ./${E}/ALPINE_REVISION`
 
     DOCKERFILE=$(docker run \
         -v ${PWD}:/ws \
         -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/Dockerfile.esh" \
-        ALPINE_VERSION=${V} \
-        ALPINE_REVISION=${ALPINE_REVISION}\
+        ALPINE_EDITION=${E} \
+        ALPINE_VERSION=${ALPINE_VERSION}\
         NUSHELL_VERSION=${NUSHELL_VERSION}
     )
 
-    echo "${DOCKERFILE}" > ./${V}/Dockerfile
-
-    if [ "${V}" = "3.8" ] || [ "${V}" = "edge" ] ; then
-        continue
-    fi
+    echo "${DOCKERFILE}" > ./${E}/Dockerfile
 
     REPOS=$(docker run \
         -v ${PWD}:/ws \
         -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/repositories.esh" \
-        ALPINE_MINOR=${V}
+        ALPINE_MINOR=${E}
     )
 
-    echo "${REPOS}" > ./${V}/overlay/etc/apk/repositories
+    echo "${REPOS}" > ./${E}/overlay/etc/apk/repositories
 
 done
 
