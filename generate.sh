94fbae8a7e06bb6fe0f26dc05757d2172a1bb40f
bfren <bf@bfren.dev>
2023-12-22 10:32:44 +0000

    Updating to Nushell 0.88.1

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 docker pull bfren/alpine
 
 BUSYBOX_VERSION="1.36.1"
-NUSHELL_VERSION="0.87.1"
+NUSHELL_VERSION="0.88.1"
 ALPINE_EDITIONS="3.15 3.16 3.17 3.18 3.19"
 
 for E in ${ALPINE_EDITIONS} ; do
